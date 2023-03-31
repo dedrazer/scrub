d500a5e077368b4244713dc5ab6a7269b841b93e
Martin Meli <martinmeli96@gmail.com>
2023-03-31 11:25:40 +0200

    update deprecated method

--- internal/entities/deck/deck_shuffle.go
+++ internal/entities/deck/deck_shuffle.go
@@ -9,7 +9,7 @@ func (d *Deck) Shuffle() {
 	d.RestoreBurnt()
 
 	for i := range d.ActiveCards {
-		rand.Seed(time.Now().UnixNano())
+		rand.New(rand.NewSource(time.Now().UnixNano()))
 		d.Swap(i, rand.Intn(len(d.ActiveCards)))
 	}
 }
